43348d3f1a9c5c13e3bcfd53cb06a014aca3e816
Nicholas LoBue <nlobue@aureusox.com>
2025-02-11 19:57:20 -0500

    Updgrading port selection logic

--- Sources/XRPLSwift/Xrpl/Clients/connection.swift
+++ Sources/XRPLSwift/Xrpl/Clients/connection.swift
@@ -286,11 +286,26 @@ public class Connection {
         }
         // Create the connection timeout, in case the connection hangs longer than expected.
         // Connection listeners: these stay attached only until a connection is done/open.
-        guard let url = url, let uri = URL(string: url), let isport = uri.port, let isscheme = uri.scheme, let ishost = uri.host else {
+        guard let url = url, let uri = URL(string: url), let isscheme = uri.scheme, let ishost = uri.host else {
             throw ConnectionError("Connection: invalid url")
         }
+
+        var port = uri.port
+        if port == nil {
+            // wss:
+            port = 443
+            
+            if isscheme == "ws" {
+                port = 80
+            }
+        }
+        
+        guard let port = port else {
+            throw ConnectionError("Connection: invalid port")
+        }
+
         let client: WebSocketClient = createWebSocket(url: url, config: self.config)!
-        try client.connect(scheme: isscheme, host: ishost, port: isport, onUpgrade: { ws -> Void in
+        try client.connect(scheme: isscheme, host: ishost, port: port, onUpgrade: { ws -> Void in
             self.ws = ws
             Task {
                 // TODO: This goes after client.connect in js, but swift doesnt(verify) return the ws. we would .wait
