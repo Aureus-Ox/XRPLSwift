3dbe750e29e4bf5759f4e94b30194393a474adfa
Nicholas LoBue <nlobue@aureusox.com>
2026-02-04 13:42:20 -0500

    Using iOS v16 since this aligns with spm versions

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:6.0
+// swift-tools-version:5.7.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
     name: "XRPLSwift",
     platforms: [
         .macOS(.v11),
-        .iOS(.v18),
+        .iOS(.v16),
         .tvOS(.v13),
         .watchOS(.v5)
     ],
