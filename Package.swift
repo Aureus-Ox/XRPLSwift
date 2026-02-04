3a5ac4c6c76d7a893c0d47a2ee003cc049131a7e
Nicholas LoBue <nlobue@aureusox.com>
2026-02-04 13:27:41 -0500

    Adding artificial delay to let the buffer clear

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7.1
+// swift-tools-version:6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
     name: "XRPLSwift",
     platforms: [
         .macOS(.v11),
-        .iOS(.v14),
+        .iOS(.v18),
         .tvOS(.v13),
         .watchOS(.v5)
     ],
